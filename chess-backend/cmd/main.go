3312cbbcc6d177853a0bf78ec94c999418856589
aljambul <jambul9827@gmail.com>
2026-05-03 17:23:37 +0500

    update cors

--- chess-backend/cmd/main.go
+++ chess-backend/cmd/main.go
@@ -40,7 +40,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"https://online-chess-tau.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
